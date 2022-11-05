ec2cd7b807421fde22a71b2a54bc9d51d9b9ff62
Matias Yanez <matias.yanez@usach.cl>
2022-11-05 15:00:26 -0300

    inicializacion del microservicio solicitud

---
+++ src/main/java/proyecto/mingeso/microservicesolicitud/MicroserviceSolicitudApplication.java
@@ -0,0 +1,13 @@
+package proyecto.mingeso.microservicesolicitud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSolicitudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSolicitudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proyecto/mingeso/microservicesolicitud/MicroserviceSolicitudApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.mingeso.microservicesolicitud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSolicitudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
