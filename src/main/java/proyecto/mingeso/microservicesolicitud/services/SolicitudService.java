97b6e8ef61b64deb495f2d625e8e98fcc09f715b
Matias Yanez <matias.yanez@usach.cl>
2022-11-06 16:59:17 -0300

    implementacion de Eureka

--- src/main/java/proyecto/mingeso/microservicesolicitud/services/SolicitudService.java
+++ src/main/java/proyecto/mingeso/microservicesolicitud/services/SolicitudService.java
@@ -22,7 +22,7 @@ public class SolicitudService {
     }
 
     public Empleado findByRut(String rut_dado){
-        Empleado empleado = restTemplate.getForObject("http://localhost:8090/empleado/byRut/" + rut_dado, Empleado.class);
+        Empleado empleado = restTemplate.getForObject("http://microservice-empleado/empleado/byRut/" + rut_dado, Empleado.class);
         return empleado;
     }
 
