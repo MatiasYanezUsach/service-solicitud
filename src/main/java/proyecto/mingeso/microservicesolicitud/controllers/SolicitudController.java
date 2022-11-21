02febfc3500b907fb260f2a053e28aa72afc3539
Matias Yanez <matias.yanez@usach.cl>
2022-11-21 10:29:11 -0300

    crossOrigin fix

--- src/main/java/proyecto/mingeso/microservicesolicitud/controllers/SolicitudController.java
+++ src/main/java/proyecto/mingeso/microservicesolicitud/controllers/SolicitudController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/solicitud")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class SolicitudController {
     @Autowired
     SolicitudService solicitudService;
