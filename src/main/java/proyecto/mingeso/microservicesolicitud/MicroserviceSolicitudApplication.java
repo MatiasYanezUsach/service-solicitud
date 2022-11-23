8dab5525b95f4ec7c4a1e0e53be7ca3310062171
Matias Yanez <matias.yanez@usach.cl>
2022-11-23 18:04:17 -0300

    test

--- src/main/java/proyecto/mingeso/microservicesolicitud/MicroserviceSolicitudApplication.java
+++ src/main/java/proyecto/mingeso/microservicesolicitud/MicroserviceSolicitudApplication.java
@@ -2,10 +2,10 @@ package proyecto.mingeso.microservicesolicitud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class MicroserviceSolicitudApplication {
 
 	public static void main(String[] args) {
